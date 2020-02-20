f995b94a8d8f4f73dd6a987b2886cf8f2d5fce27
Mempler <me@mempler.de>
2020-02-20 23:19:06 +0100

    Implement Message Indicator

--- src/App/Components/Chatbar/Components/Category/Components/Channel/Channel.tsx
+++ src/App/Components/Chatbar/Components/Category/Components/Channel/Channel.tsx
@@ -4,7 +4,7 @@ import './Channel.scss'
 import { IChannel } from '../../../../../../../API/v1/Channel';
 
 export interface ChannelProps extends Props<ChannelProps> {
-    Channel: IChannel
+    Channel: IChannel,
 }
 
 function Channel(props: ChannelProps) {
@@ -12,6 +12,10 @@ function Channel(props: ChannelProps) {
     
     return (
         <div id="Channel">
+            <svg width="6" height="8" viewBox="0 0 6 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.08806 4.01604L-0.233912 7.24441L-0.144869 0.63665L5.08806 4.01604Z" fill="white"/>
+            </svg>
+
             <span className="icon">#</span>
             <span className="name">{channel.Name.toLowerCase().replace(/ /g, "_")}</span>
         </div>
